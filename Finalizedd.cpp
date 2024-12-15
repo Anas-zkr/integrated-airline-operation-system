f663495030b9e5a9ea08365c8351c54bc2ad0886
MUHAMMAD ANAS <anasbhatti8956@gmail.com>
2024-12-15 23:49:50 +0500

    Finalized.cpp

--- Finalizedd.cpp
+++ Finalizedd.cpp
@@ -373,3 +373,378 @@ int total_cost;
         case 9:
         cout<<"Your flight will take 13 hours to reach the destination : "<<endl;
         cout<<"THE TICKET PRICE IS : $";
+        total_cost=cost_hour*13;
+        cout<<total_cost<<endl;
+            
+            break;
+
+             case 10:
+        cout<<"Your flight will take 7 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*7;
+        cout<<total_cost<<endl;
+            
+            break;
+
+           	default:
+            		return total_cost;
+                  
+      }
+break;
+      case 6:
+    
+
+        switch (arrival)
+        {
+        case 1:
+        cout<<"Your flight will take 4 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*4;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 2:
+        cout<<"Your flight will take 5 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $"<<cost_hour*26<<endl;
+         total_cost=cost_hour*5;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 3:
+        cout<<"Your flight will take 14 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $"<<cost_hour*15<<endl;
+         total_cost=cost_hour*14;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 4:
+        cout<<"Your flight will take 8 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*8;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 5:
+        cout<<"Your flight will take 2 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*2;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 7:
+        cout<<"Your flight will take 8 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*8;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 8:
+        cout<<"Your flight will take 15 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*15;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 9:
+        cout<<"Your flight will take 12 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*12;
+        cout<<total_cost<<endl;
+            
+            break;
+
+         case 10:
+        cout<<"Your flight will take 7 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*7;
+        cout<<total_cost<<endl;
+            
+            break;   
+            	default:
+            		return total_cost;
+                 
+      }
+      break;
+
+       case 7:
+    
+
+        switch (arrival)
+        {
+        case 1:
+        cout<<"Your flight will take 7 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*7;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 2:
+        cout<<"Your flight will take 6 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $"<<cost_hour*26<<endl;
+         total_cost=cost_hour*6;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 3:
+        cout<<"Your flight will take 15 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $"<<cost_hour*15<<endl;
+         total_cost=cost_hour*15;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 4:
+        cout<<"Your flight will take 11 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*11;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 5:
+        cout<<"Your flight will take 9 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*9;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 6:
+        cout<<"Your flight will take 8 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*8;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 8:
+        cout<<"Your flight will take 13 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*13;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 9:
+        cout<<"Your flight will take 13 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*13;
+        cout<<total_cost<<endl;
+            
+            break;
+        
+         case 10:
+        cout<<"Your flight will take 10 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*10;
+        cout<<total_cost<<endl;
+            
+            break;
+
+            	default:
+            		return total_cost;
+                 
+      }
+break;
+      case 8:
+    
+
+        switch (arrival)
+        {
+        case 1:
+        cout<<"Your flight will take 12 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*12;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 2:
+        cout<<"Your flight will take 13 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $"<<cost_hour*26<<endl;
+         total_cost=cost_hour*13;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 3:
+        cout<<"Your flight will take 19 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $"<<cost_hour*15<<endl;
+         total_cost=cost_hour*19;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 4:
+        cout<<"Your flight will take 21 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*21;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 5:
+        cout<<"Your flight will take 16 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*16;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 6:
+        cout<<"Your flight will take 15 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*15;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 7:
+        cout<<"Your flight will take 13 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*13;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 9:
+        cout<<"Your flight will take 17 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*17;
+        cout<<total_cost<<endl;
+            
+            break;
+
+         case 10:
+        cout<<"Your flight will take 10 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*10;
+        cout<<total_cost<<endl;
+            
+            break;
+
+            	default:
+            		return total_cost;
+                 
+      }
+      break;
+       case 9:
+    
+
+        switch (arrival)
+        {
+        case 1:
+        cout<<"Your flight will take 15 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*15;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 2:
+        cout<<"Your flight will take 16 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $"<<cost_hour*26<<endl;
+         total_cost=cost_hour*16;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 3:
+        cout<<"Your flight will take 2 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $"<<cost_hour*15<<endl;
+         total_cost=cost_hour*2;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 4:
+        cout<<"Your flight will take 8 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*8;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 5:
+        cout<<"Your flight will take 13 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*13;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 6:
+        cout<<"Your flight will take 12 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*12;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 7:
+        cout<<"Your flight will take 13 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*13;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 8:
+        cout<<"Your flight will take 17 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*17;
+        cout<<total_cost<<endl;
+            
+            break;
+
+         case 10:
+        cout<<"Your flight will take 10 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*10;
+        cout<<total_cost<<endl;
+            
+            break;
+
+            	default:
+            		return total_cost;
+                 
+      }
+      break;
+
+       case 10:
+    
+
+        switch (arrival)
+        {
+        case 1:
+        cout<<"Your flight will take 9 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*9;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 2:
+        cout<<"Your flight will take 9 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $"<<cost_hour*26<<endl;
+         total_cost=cost_hour*9;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 3:
+        cout<<"Your flight will take 9 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $"<<cost_hour*15<<endl;
+         total_cost=cost_hour*9;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 4:
+        cout<<"Your flight will take 2 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*2;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 5:
+        cout<<"Your flight will take 7 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*7;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 6:
+        cout<<"Your flight will take 7 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*7;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 7:
+        cout<<"Your flight will take 10 hours to reach the destination : "<<endl;
+        cout<<"THE TICKET PRICE IS : $";
+         total_cost=cost_hour*10;
+        cout<<total_cost<<endl;
+            
+            break;
+        case 8:
+        cout<<"Your flight will take 19 hours to reach the destination : "<<endl;
